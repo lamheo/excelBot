24cc9cb65b299e8f95553775b32184854fe9e4f0
@lam.pham2 <pham@DESKTOP-1G8L1TI.localdomain>
2022-12-28 15:24:50 +0700

    Init

--- src/main/java/koidira/product/excelFormularBot/controller/v1/public/RequestController.java
+++ src/main/java/koidira/product/excelFormularBot/controller/v1/public/RequestController.java
@@ -3,7 +3,6 @@ package koidira.product.excelFormularBot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.io.File;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -41,12 +40,7 @@ public class RequestController {
     public String init() throws Exception {
         this.driver = browserUtil.init();
         this.driver.get("https://excelformulabot.com/login?status=login");
-        browserUtil.doLogin(this.driver, "tuanit@outlook.com", "Koidira@269");
-        File SrcFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        File DestFile=new File("/home/pham/mySpace/excelFormularBot/java/excelFormularBot/testing/Temp5.png");
-
-              //Copy file at destination
-        FileUtils.copyFile(SrcFile, DestFile);
+        browserUtil.doLogin(this.driver, "nt91600@email.vccs.edu", "Koidira@269");
         return browserUtil.getCurrentUrl(this.driver);
     }
 
